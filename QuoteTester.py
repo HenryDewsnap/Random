ef54dd46b546b568f82d336aa37ab0534ddc55aa
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-01-17 01:49:08 +0000

    Create QuoteTester.py

---
+++ QuoteTester.py
@@ -0,0 +1,37 @@
+import random
+import csv
+
+FileName = "Quotes.csv"
+TotalQuotes = 3
+
+def INIT():
+	global data
+	with open('Quotes.csv', newline='') as f:
+		reader = csv.reader(f)
+		data = list(reader)
+	Q_Amm = Menu()
+	Question(Q_Amm)
+
+def Question(Q_Amm):
+	for i in range(Q_Amm):
+		Q = data[0][random.randint(0,TotalQuotes-1)]
+		Q = Q.split()
+		Missing_Wrd = random.randint(0,len(Q)-1)
+		for a in range(len(Q)-1):
+			if a == Missing_Wrd:
+				print("#####", end = " ")
+				continue
+			print(Q[a], end = " ")
+		print()
+		
+		if input("Input what the missing word is -> ") == Q[Missing_Wrd]:
+			print("Congratulations!")
+			continue
+
+		print("Unlucky")
+
+def Menu():
+	print("Quote Tester - By: HenDewsnap")
+	print(f"The file being read is [{FileName}]")
+	return int(input("How many questions would you like to be asked -> "))
+INIT()
