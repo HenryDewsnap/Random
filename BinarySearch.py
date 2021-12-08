d1ff717d7cec105278a40aaa356bdb041ef05533
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-12-08 22:51:10 +0000

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,14 @@
+def halfArray(array, target):
+    midPoint = int(len(array)/2)
+    if array[midPoint] > target:
+        return array[0:midPoint]
+    return array[midPoint::]
+
+def binarySearch(data, target):
+    while len(data) > 2:
+        print(data)
+        data = halfArray(data, target)
+    if target in data: print(f"Found: {target}!")
+    else: print(f"{target} not found! Closest values: {data}")
+
+binarySearch([0,23,54,100,1232,4324,54903,432000], int(input("Target: ")))
