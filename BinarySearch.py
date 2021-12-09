5d6baab42bad70f473d85c11b8cfb2d5c5e53952
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-12-09 18:49:29 +0000

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -8,7 +8,7 @@ def binarySearch(data, target):
     while len(data) > 2:
         print(data)
         data = halfArray(data, target)
-    if target in data: print(f"Found: {target}!")
+    if target == data[0] or target == data[1]: print(f"Found: {target}!")
     else: print(f"{target} not found! Closest values: {data}")
 
 binarySearch([0,23,54,100,1232,4324,54903,432000], int(input("Target: ")))
